52082ca920125210e433ae6bb028c13a7941572c
rafaellevy <srlevy2@gmail.com>
2021-09-24 11:37:18 -0700

    SiftUp and remove methods added to MinHeap class

--- HuffmanCoding.py
+++ HuffmanCoding.py
@@ -6,6 +6,13 @@ class Node:
         self.key = key
         self.value = value
 
+class HuffNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
 class MinHeap:
     def __init__(self, array):
         self.heap = self.buildHeap(array)
@@ -20,20 +27,61 @@ class MinHeap:
         childOneIdx = currentIdx * 2 + 1
         while childOneIdx <= endIdx:
             childTwoIdx = currentIdx * 2 + 2 if currentIdx * 2 + 2 <= endIdx else -1
-            if childTwoIdx != -1 and heap[childTwoIdx] < heap[childOneIdx]:
+            if childTwoIdx != -1 and heap[childTwoIdx].value < heap[childOneIdx].value:
                 idxToSwap = childTwoIdx
             else:
                 idxToSwap = childOneIdx
 
-            if heap[idxToSwap] < heap[currentIdx]:
+            if heap[idxToSwap].value < heap[currentIdx].value:
                 self.swap(currentIdx, idxToSwap, heap)
                 currentIdx = idxToSwap
                 childOneIdx = currentIdx * 2 + 1
             else:
                 return
 
+    def siftUp(self, currentIdx, heap):
+        parentIdx = (currentIdx - 1) //2
+        while currentIdx > 0 and heap[currentIdx].value < heap[parentIdx].value:
+            self.swap(currentIdx, parentIdx, heap)
+            currentIdx = parentIdx
+            parentIdx = (currentIdx - 1) // 2
+
     def swap(self, i, j, heap):
-        heap[i], heap[j] = heap[j], heap[i]   
+        heap[i], heap[j] = heap[j], heap[i]
+    
+    def remove(self):
+        self.swap(0, len(self.heap) -1, self.heap)
+        valueToRemove = self.heap.pop()
+        self.siftDown(0, len(self.heap) -1, self.heap)
+        return valueToRemove
+
+    def insert(self, value):
+        self.heap.append(value)
+        self.siftUp(len(self.heap) - 1, self.heap)
+    
+
+def buildHuffTree(array):
+    # MinHeap object
+    min_heap = MinHeap(array)
+    hoff_tree = []
+
+    while len(min_heap.heap) > 1:
+        # remove the root node of heap
+        node1 = min_heap.remove()
+        # remove the root node of heap
+        node2 = min_heap.remove()
+        node3 = HuffNode(node1.value + node2.value)
+        node3.left = node1
+        node3.right = node2
+        hoff_tree.append(node3)
+        min_heap.insert(node3)
+
+    print([x.value for x in hoff_tree])
+
+    
+
+
+
 
 def huffman_encoding(data):
     #counter returns a dictionary 
@@ -45,9 +93,9 @@ def huffman_encoding(data):
     for node in frequencyArray:
         print(node.key, node.value)
     print()
-    minHeap = MinHeap(frequencyArray)
-    for node in minHeap.heap:
-        print(node.key, node.value)
+
+    print()
+    buildHuffTree(frequencyArray)
 
 huffman_encoding("AAATBBSIS")
 
